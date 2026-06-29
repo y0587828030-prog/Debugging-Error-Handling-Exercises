c4195272fe9da68d1d335e23bc230d3260b433ec
yehoshua <y0587828030@gmail.com>
2026-06-29 15:45:24 +0300

    1 - Error - Enter only a number

--- debugging.py
+++ debugging.py
@@ -1,2 +1,11 @@
+##Python Debugging + Error Handling Exercises
+#step 1
+age = input("Enter your age: ")
+try:
+    next_year = int(age) + 1
+    print("Next year you will be", next_year)
+except:
+    print("Age must be a number.")
 
-print ("hi")
+
+#ValueError:
