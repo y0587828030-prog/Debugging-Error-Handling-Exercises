40e07f662ef1189f5d602bae9e446403837bd8ee
yehoshua <y0587828030@gmail.com>
2026-06-29 15:59:17 +0300

    3 Error - Index not found

--- debugging.py
+++ debugging.py
@@ -1,19 +1,26 @@
-##Python Debugging + Error Handling Exercises
+#Python Debugging + Error Handling Exercises
 # #step 1
 # age = input("Enter your age: ")
 # try:
 #     next_year = int(age) + 1
 #     print("Next year you will be", next_year)
-# except:
+# except ValueError:
 #     print("Age must be a number.")
-# #ValueError:
 
 
-#step 2
-a = int(input("First number: "))
-b = int(input("Second number: "))
+
+# #step 2
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# try:
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero.")
+
+#step 3
+numbers = [10, 20, 30]
+index = int(input("Choose index: "))
 try:
-    print(a / b)
-except:
-    print("Cannot divide by zero.")
-#ZeroDivisionError
+    print(numbers[index])
+except IndexError:
+    print("Index not found.")
