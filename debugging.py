f3fc4eb75da64d5d13a511935fafd995416ac939
yehoshua <y0587828030@gmail.com>
2026-06-29 15:51:30 +0300

    2 -Error - division by 0

--- debugging.py
+++ debugging.py
@@ -1,11 +1,19 @@
 ##Python Debugging + Error Handling Exercises
-#step 1
-age = input("Enter your age: ")
+# #step 1
+# age = input("Enter your age: ")
+# try:
+#     next_year = int(age) + 1
+#     print("Next year you will be", next_year)
+# except:
+#     print("Age must be a number.")
+# #ValueError:
+
+
+#step 2
+a = int(input("First number: "))
+b = int(input("Second number: "))
 try:
-    next_year = int(age) + 1
-    print("Next year you will be", next_year)
+    print(a / b)
 except:
-    print("Age must be a number.")
-
-
-#ValueError:
+    print("Cannot divide by zero.")
+#ZeroDivisionError
